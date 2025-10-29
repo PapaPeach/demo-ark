b7a308f22f3e6e529e331816d621dec9bd7fa05f
Peaches <peachespear3@gmail.com>
2025-10-29 01:47:06 -0400

    Added gametype sorting and reformatted

--- main.go
+++ main.go
@@ -1,109 +1,136 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"slices"
-
-	"github.com/pektezol/bitreader"
+	"strings"
 )
 
-func readString(file io.Reader, length int) string {
-	// Only read desired length
-	rawString := make([]byte, length)
-	io.ReadFull(file, rawString)
-
-	// Return without null characters
-	for i, character := range rawString {
-		if character == 0 {
-			return string(rawString[:i])
-		}
+/** Checks if conVar exists with the desired value in a array conVars */
+func checkConVar(conVars []string, wishStr string, wishVal string) bool {
+	if i := slices.Index(conVars, wishStr); i != -1 && conVars[i+1] == wishVal {
+		return true
 	}
-
-	return string(rawString[:])
+	return false
 }
 
-func readVarInt(bitReader *bitreader.Reader) uint32 {
-	var result uint32 = 0
-	var shift uint = 0
+/** Returns a list of .dem files in the current directory */
+func getDemos() []string {
+	// Get list of files in current directory
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 
-	// Maximum 5 bytes = 5 * 7 bits = 35 bits
-	for shift = 0; shift < 35; shift += 7 {
-		// Assume the var int is byte-aligned, so read 8 bits at once
-		varInt, err := bitReader.ReadBits(8)
-		if err != nil {
-			return 0
-		}
-		b := uint8(varInt)
-
-		result |= uint32(b&0x7F) << shift
-
-		if b&0x80 == 0 {
-			break
+	// Filter list to only have .dem files
+	var demos []string
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".dem") {
+			demos = append(demos, file.Name())
 		}
 	}
 
-	return result
+	return demos
+}
+
+/** Moves files in a list of files to a directory */
+func moveToDirectory(wishDir string, files []string) {
+	// Handle length accordingly
+	length := len(files)
+	switch length {
+	case 0: // Skip if no files to move exist
+		return
+	case 1: // Singular demo file
+		fmt.Printf("Moving %d demo to %s...\n", length, wishDir)
+	default: // Plural demos
+		fmt.Printf("Moving %d demos to %s...\n", length, wishDir)
+	}
+
+	// Make directory to move to
+	err := os.Mkdir(wishDir, os.ModePerm)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// Move files to directory
+	for _, file := range files {
+		err := os.Rename(file, filepath.Join(wishDir, file))
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+/** Prompts user to press enter to exit the program, then exit when entered */
+func enterToExit() {
+	fmt.Println("Press enter to close.")
+	fmt.Scanln()
+	os.Exit(0)
 }
 
 func main() {
-	filename := "official.dem"
+	// Get demos
+	demos := getDemos()
+	fmt.Printf("Scanning %d demos...\n", len(demos))
 
-	// Open file for reading
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("Error opening %v: %v", filename, err)
-	}
-	defer file.Close()
+	// Scan demos and determine gamemode type
+	var casualDemos []string
+	var mvmDemos []string
+	var tournamentDemos []string
+	for _, filename := range demos {
+		// Open file for reading
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Printf("Error opening %v: %v", filename, err)
+		}
 
-	// Get header
-	header := readHeader(file)
-
-	// Print header
-	printHeader(header)
-
-	// Get message
-	msg := readMessage(file)
-	printMessage(msg)
-
-	// Determine if casual
-	casual := false
-	for _, sc := range msg.ParsedData.SetConVar {
-		if i := slices.Index(sc.ConVars, "mp_tournament"); i != -1 && sc.ConVars[i+1] == "1" {
-			fmt.Println("mp_tournament 1")
-		} else {
+		// Determine if MvM via map prefix in header
+		header := readHeader(file)
+		if strings.HasPrefix(header.MapName, "mvm_") {
+			//fmt.Println("Detected mvm demo:\t\t", filename)
+			mvmDemos = append(mvmDemos, filename)
+			file.Close()
 			continue
 		}
 
-		if i := slices.Index(sc.ConVars, "mp_tournament_stopwatch"); i != -1 && sc.ConVars[i+1] == "0" {
-			fmt.Println("mp_tournament_stopwatch 0")
-		} else {
-			continue
-		}
+		// Get message contents
+		file.Seek(1072, io.SeekStart)
+		msg := readMessage(file)
 
-		if i := slices.Index(sc.ConVars, "mp_tournament_readymode"); i != -1 && sc.ConVars[i+1] == "1" {
-			fmt.Println("mp_tournament_readymode 1")
-		} else {
-			continue
+		// Determine if casual via specific conVar values
+		casual := false
+		for _, sc := range msg.ParsedData.SetConVar {
+			// Check if tournament 1, stopwatch 0, tournament_readymode_min 0
+			if checkConVar(sc.ConVars, "mp_tournament", "1") &&
+				checkConVar(sc.ConVars, "mp_tournament_stopwatch", "0") &&
+				checkConVar(sc.ConVars, "mp_tournament_readymode", "1") &&
+				checkConVar(sc.ConVars, "mp_tournament_readymode_min", "0") {
+				casual = true
+				file.Close()
+				break
+			}
 		}
-
-		if i := slices.Index(sc.ConVars, "mp_tournament_readymode_min"); i != -1 && sc.ConVars[i+1] == "0" {
-			fmt.Println("mp_tournament_readymode_min 0")
+		if casual {
+			//fmt.Println("Detected casual demo:\t\t", filename)
+			casualDemos = append(casualDemos, filename)
 		} else {
-			continue
+			//fmt.Println("Detected tournament demo:\t", filename)
+			tournamentDemos = append(tournamentDemos, filename)
 		}
-
-		casual = true
-		break
-	}
-	if casual {
-		fmt.Println("Detected casual demo")
-	} else {
-		fmt.Println("Detected tournament demo")
+		file.Close()
 	}
 
-	// Get current position in file (should be 1072)
-	/*pos, _ := file.Seek(0, io.SeekCurrent)
-	fmt.Println(pos)*/
+	// Move files to corresponding directories
+	moveToDirectory("demos_casual", casualDemos)
+	moveToDirectory("demos_mvm", mvmDemos)
+	moveToDirectory("demos_tournament", tournamentDemos)
+
+	//enterToExit()
 }
