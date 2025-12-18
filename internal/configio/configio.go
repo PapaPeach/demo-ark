d7ef80c313ea27a11a51105d9d73e72e4afdbfbf
Peaches <peachespear3@gmail.com>
2025-12-18 13:45:50 -0500

    Updated Linux support
    Split shortcut packages by OS for cross-compilation support. Updated .desktop shortcut generation for Linux.

--- internal/configio/configio.go
+++ internal/configio/configio.go
@@ -6,14 +6,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"reflect"
 	"runtime"
 	"slices"
 	"strconv"
 	"strings"
-
-	"github.com/jxeng/shortcut"
 )
 
 type Arguments struct {
@@ -595,129 +591,6 @@ func LaunchGame() {
 	if err != nil {
 		log.Println("Error launching TF2:", err)
 		util.EnterToExit(false)
-	}
-}
-
-/* Creates a shortcut to run the program with the currently applied options. */
-func CreateConfiguredShortcut(args Arguments) {
-	// Get filepath of program
-	programPath, err := os.Executable()
-	if err != nil {
-		log.Println("Error getting program path for shortcut:", err)
-		util.EnterToExit(false)
-	}
-
-	// Get currently applied options
-	argsString := "Silent=1"
-	argsTypes := reflect.TypeOf(args)
-	argsValues := reflect.ValueOf(args)
-	for i := 1; i < argsTypes.NumField(); i++ {
-		// Format currently applied options as expected arguments
-		value := 0
-		switch argsValues.Field(i).Kind() {
-		case reflect.Bool:
-			// Skip create shortcut
-			if strings.EqualFold(argsTypes.Field(i).Name, CreateShortcut) {
-				continue
-			}
-			// Simplify booleans arguments
-			if argsValues.Field(i).Bool() {
-				value = 1
-			}
-			argsString += fmt.Sprintf(" %s=%d", argsTypes.Field(i).Name, value)
-
-		case reflect.Uint8: // Int arguments
-			fallthrough
-		case reflect.Uint16:
-			argsString += fmt.Sprintf(" %s=%v", argsTypes.Field(i).Name, argsValues.Field(i))
-
-		case reflect.String: // String arguments
-			if len(argsValues.Field(i).String()) == 0 {
-				continue
-			}
-			argsString += fmt.Sprintf(" %s=%s", argsTypes.Field(i).Name, argsValues.Field(i))
-
-		case reflect.Slice: // Slice argument
-			if argsValues.Field(i).Len() == 0 {
-				continue
-			}
-			argsString += fmt.Sprintf(" %s=", argsTypes.Field(i).Name)
-			for j := range argsValues.Field(i).Len() {
-				argsString += fmt.Sprintf("%s ", argsValues.Field(i).Index(j))
-			}
-		default:
-			log.Println("Error getting arguments for shortcut", err)
-			util.EnterToExit(false)
-		}
-	}
-
-	// Get filepath to TF2's game.ico
-	iconPath, err := os.Getwd()
-	if err != nil {
-		log.Println("Error getting working directory for shortcut icon:", err)
-	}
-
-	// Make sure path is in tf directory
-	tf := filepath.Join("Team Fortress 2", "tf")
-	tfIndex := strings.Index(iconPath, tf)
-	if tfIndex == -1 {
-		fmt.Println("Program is not currently in a subdirectory of tf, cannot locate TF2 icon.")
-	}
-	iconPath = filepath.Join(iconPath[:tfIndex+len(tf)], "resource")
-
-	// Get correct icon for os
-	switch runtime.GOOS {
-	case "windows":
-		iconPath = filepath.Join(iconPath, "game.ico")
-	case "linux":
-		fallthrough
-	case "darwin": // Mac
-		iconPath = filepath.Join(iconPath, "game.icns")
-	default:
-		log.Println("Detected unsupported operating system.")
-		util.EnterToExit(false)
-	}
-
-	// Create shortcut
-	shortcutPath := filepath.Join(".", "DemoArk Shortcut")
-	switch runtime.GOOS {
-	case "windows": // Create .lnk shortcut
-		fmt.Println("Creating configured Windows shortcut...")
-		sc := shortcut.Shortcut{
-			ShortcutPath:     shortcutPath + ".lnk",
-			Target:           programPath,
-			IconLocation:     iconPath,
-			Arguments:        argsString,
-			Description:      "",
-			Hotkey:           "",
-			WindowStyle:      "1",
-			WorkingDirectory: "",
-		}
-		err = shortcut.Create(sc)
-		if err != nil {
-			log.Println("Error creating Windows shortcut:", err)
-			util.EnterToExit(false)
-		}
-	case "linux": // Create .desktop shortcut
-		fmt.Println("Creating configured Linux shortcut...")
-		sc, err := os.Create(shortcutPath + ".desktop")
-		if err != nil {
-			log.Println("Error creating Linux shortcut:", err)
-		}
-		defer sc.Close()
-
-		// Write contents of .desktop file
-		sc.WriteString("[Desktop Entry]\n")
-		sc.WriteString("Encoding=UTF-8\n")
-		sc.WriteString("Version=1.0\n")
-		sc.WriteString("Type=Application\n")
-		sc.WriteString("Terminal=false\n")
-		sc.WriteString("Exec=" + programPath + "\n")
-		sc.WriteString("Name=DemoArk\n")
-		sc.WriteString("Icon=" + iconPath + "\n")
-	case "darwin": // Mac: Just print path for copy + paste
-		fmt.Println("Creating copy+paste-able Mac path...")
-		fmt.Printf("%s %s\nPath to icon: %s\n", programPath, argsString, iconPath)
 	}
 }
 
---
+++ internal/configio/shortcut_other.go
@@ -0,0 +1,127 @@
+//go:build !windows
+
+package configio
+
+import (
+	"demo-ark/demoark/internal/util"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+/* Creates a shortcut to run the program with the currently applied options. */
+func CreateConfiguredShortcut(args Arguments) {
+	// Get filepath of program
+	programPath, err := os.Executable()
+	if err != nil {
+		log.Println("Error getting program path for shortcut:", err)
+		util.EnterToExit(false)
+	}
+
+	// Get currently applied options
+	argsString := "Silent=1"
+	argsTypes := reflect.TypeOf(args)
+	argsValues := reflect.ValueOf(args)
+	for i := 1; i < argsTypes.NumField(); i++ {
+		// Format currently applied options as expected arguments
+		value := 0
+		switch argsValues.Field(i).Kind() {
+		case reflect.Bool:
+			// Skip create shortcut
+			if strings.EqualFold(argsTypes.Field(i).Name, CreateShortcut) {
+				continue
+			}
+			// Simplify booleans arguments
+			if argsValues.Field(i).Bool() {
+				value = 1
+			}
+			argsString += fmt.Sprintf(" %s=%d", argsTypes.Field(i).Name, value)
+
+		case reflect.Uint8: // Int arguments
+			fallthrough
+		case reflect.Uint16:
+			argsString += fmt.Sprintf(" %s=%v", argsTypes.Field(i).Name, argsValues.Field(i))
+
+		case reflect.String: // String arguments
+			if len(argsValues.Field(i).String()) == 0 {
+				continue
+			}
+			argsString += fmt.Sprintf(" %s=%s", argsTypes.Field(i).Name, argsValues.Field(i))
+
+		case reflect.Slice: // Slice argument
+			if argsValues.Field(i).Len() == 0 {
+				continue
+			}
+			argsString += fmt.Sprintf(" %s=", argsTypes.Field(i).Name)
+			for j := range argsValues.Field(i).Len() {
+				argsString += fmt.Sprintf("%s ", argsValues.Field(i).Index(j))
+			}
+		default:
+			log.Println("Error getting arguments for shortcut", err)
+			util.EnterToExit(false)
+		}
+	}
+
+	// Get filepath to TF2's game.ico
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println("Error getting working directory for shortcut icon:", err)
+	}
+
+	// Make sure path is in tf directory
+	tf := filepath.Join("Team Fortress 2", "tf")
+	iconPath := path
+	tfIndex := strings.Index(iconPath, tf)
+	if tfIndex == -1 {
+		fmt.Println("Program is not currently in a subdirectory of tf, cannot locate TF2 icon.")
+	}
+	iconPath = filepath.Join(iconPath[:tfIndex+len(tf)], "resource")
+
+	// Get correct icon for os
+	switch runtime.GOOS {
+	case "linux":
+		iconPath = filepath.Join(iconPath, "game.ico")
+	case "darwin": // Mac
+		iconPath = filepath.Join(iconPath, "game.icns")
+	default:
+		log.Println("Detected unsupported operating system.")
+		util.EnterToExit(false)
+	}
+
+	// Create shortcut
+	shortcutPath := filepath.Join(".", "DemoArk Shortcut")
+	switch runtime.GOOS {
+	case "linux": // Create .desktop shortcut
+		fmt.Println("Creating configured Linux shortcut...")
+		sc, err := os.Create(shortcutPath + ".desktop")
+		if err != nil {
+			log.Println("Error creating Linux shortcut:", err)
+		}
+		defer sc.Close()
+
+		// Write contents of .desktop file
+		sc.WriteString("[Desktop Entry]\n")
+		sc.WriteString("Type=Application\n")
+		sc.WriteString("Version=1.0\n")
+
+		sc.WriteString("Name=Demo Ark\n")
+		sc.WriteString("Comment=Demo archiver with user configured options\n")
+		sc.WriteString("Icon=" + iconPath + "\n")
+		sc.WriteString("Path=" + path + "\n")
+		sc.WriteString("Exec=\"" + programPath + "\"" + argsString + "\n")
+
+		sc.WriteString("Terminal=false\n")
+		sc.WriteString("Categories=Games;Utility;Application;\n")
+
+	case "darwin": // Mac: Just print path for copy + paste
+		fmt.Println("Creating copy+paste-able Mac path...")
+		fmt.Printf("%s %s\nPath to icon: %s\n", programPath, argsString, iconPath)
+	default:
+		log.Println("Detected unsupported operating system.")
+		util.EnterToExit(false)
+	}
+}
